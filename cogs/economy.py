da489f2b27b33c2b78f5bc7f2afa483d407f6184
qBaumi <max05posch@gmail.com>
2022-04-09 23:36:25 +0200

    change redeem mention for admins

--- cogs/economy.py
+++ cogs/economy.py
@@ -350,9 +350,10 @@ class economy(commands.Cog):
                 supportchannel = await self.client.fetch_channel(support_channel_id)
                 await interaction.channel.send(f"{user.mention}\n**To redeem your prize please open a ticket in {supportchannel.mention}")
                 channel = await self.client.fetch_channel(598608660125581362) # Send message to an admin channel or idk
-                guild = await self.client.fetch_guild(598303095352459305) # fetch admin role and mention it
-                adminrole = guild.get_role(598307015181467650)
-                await channel.send(f"{adminrole}\n{user.mention} claimed {item}!")
+                noti = await self.client.fetch_user(198218633955115008) # fetch noti, ing and vain
+                ing = await self.client.fetch_user(497508029923852299) # fetch noti, ing and vain
+                vain = await self.client.fetch_user(253538064519921664) # fetch noti, ing and vain
+                await channel.send(f"{noti.mention}{ing.mention}{vain.mention}\n{user.mention} | {user} claimed {item}!")
                 break
             index+=1
 
