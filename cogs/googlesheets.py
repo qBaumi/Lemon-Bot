8381a937062b10990cc4c836135950bc62692556
qBaumi <max05posch@gmail.com>
2022-04-27 19:55:07 +0200

    send message on locking

--- cogs/googlesheets.py
+++ cogs/googlesheets.py
@@ -73,6 +73,8 @@ class googlesheets(commands.Cog):
                     print(range["time"])
                     if datetime.datetime.now().strftime("%d-%m-%Y") == range["date"] and range["time"] == datetime.datetime.now().strftime("%H:%M"):
                         self.lock_sheet(range)
+                        channel = await self.client.fetch_channel(963720915575779358)
+                        await channel.send("Sheet was succesfully locked!")
                         await asyncio.sleep(1)
                 await asyncio.sleep(59)
     def lock_sheet(self, range):
