8315f48d4203414031fbe9290dd79eac67277331
qBaumi <max05posch@gmail.com>
2022-10-09 14:06:11 +0200

    remove vc chats warning

--- cogs/other.py
+++ cogs/other.py
@@ -20,6 +20,8 @@ class other(commands.Cog):
         self.client = client
         self.logchannel = None
 
+
+
     @commands.Cog.listener()
     async def on_ready(self):
         self.logchannel = await self.client.fetch_channel(967113937634078771)
@@ -102,6 +104,13 @@ class other(commands.Cog):
         await channel.set_permissions(guild.default_role, view_channel=False)
         await channel.set_permissions(role, view_channel=True)
 
+    @commands.command()
+    async def badge(self, ctx):
+        guild = await self.client.fetch_guild(598303095352459305)
+        role = discord.utils.get(guild.roles, id=598307062086107156)
+        print(role.icon)
+        await ctx.send(role.icon)
+
 
     @commands.Cog.listener()
     async def on_member_update(self, beforemember, aftermember):
@@ -134,12 +143,12 @@ class other(commands.Cog):
             em.add_field(name=message.channel.name, value=message.content)
             await self.logchannel.send(embed=em)
 
-            badwords = ["fag", "farggot", "nigger", "nazi", "cancer", "autist", "retard", "jew", "mentally challenged", "suicide", "kill myself", "kill yourself", "autistic", "kys", "kms", "dick", "cock", "porn", "slut", "niglet", "negro", "dyke", "whore", "chink", "nibba", "braindead", "autism", "nigglet", "beaner", "child-fucker", "motherfucker", "twat", "bellend", "arschgesicht", "rape", "cocknose", "anal", "nonce", "depression", "rape", "rapist", "aids", "nigga", "pedo", "subhuman", "kike", "kyke", "nignog", "cao ni ma", "ape"]
-            if any(word in message.content.lower() for word in badwords):
-                modchannel = await self.client.fetch_channel(963720915575779358)
-                em = discord.Embed(title="Warning", description=f"<#967113937634078771> for more information")
-                em.add_field(name=message.author.name, value=message.content)
-                await modchannel.send(embed=em)
+            #badwords = ["fag", "farggot", "nigger", "nazi", "cancer", "autist", "retard", "jew", "mentally challenged", "suicide", "kill myself", "kill yourself", "autistic", "kys", "kms", "dick", "cock", "porn", "slut", "niglet", "negro", "dyke", "whore", "chink", "nibba", "braindead", "autism", "nigglet", "beaner", "child-fucker", "motherfucker", "twat", "bellend", "arschgesicht", "rape", "cocknose", "anal", "nonce", "depression", "rape", "rapist", "aids", "nigga", "pedo", "subhuman", "kike", "kyke", "nignog", "cao ni ma", "ape"]
+            #if any(word in message.content.lower() for word in badwords):
+            #    modchannel = await self.client.fetch_channel(963720915575779358)
+            #    em = discord.Embed(title="Warning", description=f"<#967113937634078771> for more information")
+            #    em.add_field(name=message.author.name, value=message.content)
+            #    await modchannel.send(embed=em)
 
 
 
