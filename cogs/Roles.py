91da69b3b3155ab01fe617b6df1592229f508a61
qBaumi <max05posch@gmail.com>
2023-01-31 14:36:51 +0100

    test

--- cogs/Roles.py
+++ cogs/Roles.py
@@ -10,6 +10,16 @@ from discord.ext import commands
 from discord import app_commands
 from config import guilds
 
+roles = [
+    {"category": "lolesports", "roles": [
+        {"name": "Pseudo Lolesports Chatter", "tier": 1},
+        {"name": "Lolesports Chatter", "tier": 2}
+    ]},
+    {"category": "general", "roles": [
+        {"name": "General Chatter", "tier": 1},
+        {"name": "Silver Player", "tier": 2}
+    ]}
+]
 
 
 class Roles(commands.GroupCog):
