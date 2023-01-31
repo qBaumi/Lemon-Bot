ddb3c5e13c12b9d60a1ff863606665b92114676c
qBaumi <max05posch@gmail.com>
2023-01-31 14:32:36 +0100

    Update Roles.py

--- cogs/Roles.py
+++ cogs/Roles.py
@@ -10,16 +10,6 @@ from discord.ext import commands
 from discord import app_commands
 from config import guilds
 
-roles = [
-    {"category": "lolesports", "roles": [
-        {"name": "Pseudo Lolesports Chatter", "tier": 1},
-        {"name": "Lolesports Chatter", "tier": 2}
-    ]},
-    {"category": "general", "roles": [
-        {"name": "General Chatter", "tier": 1},
-        {"name": "Silver Player", "tier": 2}
-    ]}
-]
 
 
 class Roles(commands.GroupCog):
@@ -33,13 +23,22 @@ class Roles(commands.GroupCog):
     # activate role
     # deactivate role
 
-
+    roles = [
+        {"category": "lolesports", "roles": [
+            {"name": "Pseudo Lolesports Chatter", "tier": 1},
+            {"name": "Lolesports Chatter", "tier": 2}
+        ]},
+        {"category": "general", "roles": [
+            {"name": "General Chatter", "tier": 1},
+            {"name": "Silver Player", "tier": 2}
+        ]}
+    ]
 
     @app_commands.command(name="shop", description="Show all roles that you can buy")
     async def shop(self, interaction: discord.Interaction):
         embed = discord.Embed(title="Roles Tier 1",
                               description="You can buy Tier 1 roles here which you can then further upgrade for more money.")
-        for category in roles:
+        for category in self.roles:
             for role in category["roles"]:
                 embed.add_field(name=role["name"], value=f"Tier {role['tier']}")
         await interaction.response.send_message(embed=embed)
@@ -51,7 +50,7 @@ class Roles(commands.GroupCog):
             return
         embed = discord.Embed(title="Roles Tier 1",
                               description="You can buy Tier 1 roles here which you can then further upgrade for more money.")
-        for category in roles:
+        for category in self.roles:
             for role in category["roles"]:
                 embed.add_field(name=role["name"], value=f"Tier {role['tier']}")
         await interaction.response.send_message(embed=embed)
@@ -71,7 +70,7 @@ class Roles(commands.GroupCog):
     async def getAvailableRoles(self, userid):
         userCategories = es.sql_select(f"SELECT category FROM roles WHERE id = '{userid}'")
         availableRoles = []
-        for category in roles:
+        for category in self.roles:
             if not category["category"] in userCategories:
                 availableRoles.append(category["roles"][0])
         return availableRoles
