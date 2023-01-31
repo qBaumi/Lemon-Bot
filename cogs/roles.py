9156ca56a445e45cde6ce9a7f4498684d55c019d
qBaumi <max05posch@gmail.com>
2023-01-31 14:21:08 +0100

    test

--- cogs/roles.py
+++ cogs/roles.py
@@ -33,47 +33,5 @@ class Roles(commands.GroupCog):
     # activate role
     # deactivate role
 
-    @app_commands.command(name="shop", description="Show all roles that you can buy")
-    async def shop(self, interaction: discord.Interaction):
-        embed = discord.Embed(title="Roles Tier 1",
-                              description="You can buy Tier 1 roles here which you can then further upgrade for more money.")
-        for category in roles:
-            for role in category["roles"]:
-                embed.add_field(name=role["name"], value=f"Tier {role['tier']}")
-        await interaction.response.send_message(embed=embed)
-
-    @app_commands.command(name="buy", description="Buy a Tier 1 role")
-    @app_commands.describe(role="The Tier 1 role you want to buy")
-    async def buy(self, interaction: discord.Interaction, role: str):
-        if not await es.interaction_check_account(interaction):
-            return
-        embed = discord.Embed(title="Roles Tier 1",
-                              description="You can buy Tier 1 roles here which you can then further upgrade for more money.")
-        for category in roles:
-            for role in category["roles"]:
-                embed.add_field(name=role["name"], value=f"Tier {role['tier']}")
-        await interaction.response.send_message(embed=embed)
-
-    @buy.autocomplete('roles')
-    async def buy_autocomplete(
-            self,
-            interaction: discord.Interaction,
-            current: str
-    ) -> List[app_commands.Choice[str]]:
-        availableRoles = await self.getAvailableRoles(interaction.user.id)
-        return [
-            app_commands.Choice(name=role, value=role.lower())
-            for role in availableRoles if current.lower() in role.lower()
-        ]
-
-    async def getAvailableRoles(self, userid):
-        userCategories = es.sql_select(f"SELECT category FROM roles WHERE id = '{userid}'")
-        availableRoles = []
-        for category in roles:
-            if not category["category"] in userCategories:
-                availableRoles.append(category["roles"][0])
-        return availableRoles
-
-
 async def setup(client):
     await client.add_cog(Roles(client), guilds=guilds)
