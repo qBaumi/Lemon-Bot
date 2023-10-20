e30da2c7b3f85441e1950ffaa08eb511a6f763a7
qBaumi <max05posch@gmail.com>
2023-10-20 23:42:47 +0200

    Update prediction.py

--- cogs/prediction.py
+++ cogs/prediction.py
@@ -390,11 +390,14 @@ async def getAllPredictionsByUser(interaction, user):
     WHERE userid = '{user.id}'
     ORDER BY timestamp
     """)
-    str = f"{user} currently has **{getPointsByUserId(user.id)}** points\n\n"
+    str = f"{user} currently has **{getPointsByUserId(user.id)}** points\n"
     last_date = datetime.date.fromtimestamp(int(mypredictions[0][4].decode('utf-8')))
-    print(f"{last_date.day}-{last_date.month}")
+    #print(f"{last_date.day}-{last_date.month}")
     for prediction in mypredictions:
-        timestamp = prediction[4].decode('utf-8')
+        day_month = f"{datetime.date.fromtimestamp(int(prediction[4].decode('utf-8'))).day}-{datetime.date.fromtimestamp(int(prediction[4].decode('utf-8'))).month}"
+        if f"{last_date.day}-{last_date.month}" != day_month:
+            last_date = day_month
+            str += f"\n**{day_month}**\n"
         str += f"**{prediction[2].decode('utf-8')}** vs **{prediction[3].decode('utf-8')}** | **{prediction[0]}** - **{prediction[1]}**\n"
     em = discord.Embed(title=f"All Predictions of {user}", colour=discord.Color.dark_red(), description=str)
     await interaction.response.send_message(embed=em, ephemeral=True)
