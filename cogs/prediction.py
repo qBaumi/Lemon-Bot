e83b1f516e185b1847cc1ab4a6289663d50a0f63
qBaumi <max05posch@gmail.com>
2023-10-20 23:46:02 +0200

    Update prediction.py

--- cogs/prediction.py
+++ cogs/prediction.py
@@ -392,7 +392,7 @@ async def getAllPredictionsByUser(interaction, user):
     """)
     str = f"{user} currently has **{getPointsByUserId(user.id)}** points\n"
     last_date = datetime.date.fromtimestamp(int(mypredictions[0][4].decode('utf-8')))
-    str += f"**{last_date.day}-{last_date.month}\n**"
+    str += f"**{last_date.day}-{last_date.month}**\n"
     #print(f"{last_date.day}-{last_date.month}")
     for prediction in mypredictions:
         day_month = f"{datetime.date.fromtimestamp(int(prediction[4].decode('utf-8'))).day}-{datetime.date.fromtimestamp(int(prediction[4].decode('utf-8'))).month}"
