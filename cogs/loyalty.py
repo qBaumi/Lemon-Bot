4413c7d0694c99c12362b4d10958db649f69af3c
Vio-76 <263196134+Vio-76@users.noreply.github.com>
2026-02-28 12:43:38 +0100

    Added rewardchannel command
    def _add_loyalty_points() is copied and modified from reward command, would be better to refactor it such that it provides shared functionality for reward and rewardchannel but can't do it it myself right now for reasons

--- cogs/loyalty.py
+++ cogs/loyalty.py
@@ -64,6 +64,50 @@ class loyalty(commands.Cog):
         await interaction.response.send_message(
             f"{user.mention} got {points} loyalty points, they now have {prevpoints + points} loyalty points in total!")
 
+    async def _add_loyalty_points(self, interaction: discord.Interaction, user: discord.User, points: int):
+        # If the user has no loyalty points he gets inserted into the loyalty table
+        if not self.check_loyalty(user):
+            es.sql_exec(f"INSERT INTO loyalty (id, points) VALUES ('{user.id}', {points})")
+
+            await interaction.followup.send(
+                f"{user.mention} got {points} loyalty points, they now have {points} loyalty points in total!")
+
+            # Now check if user already startupped
+            ids = es.sql_select("SELECT id FROM users")
+            for id in ids:
+                if str(interaction.user.id) == id[0]:
+                    return
+            # If he didnt startup also give them the startup money
+            await es.update_balance(user, 50)
+            return
+        # If the user already received loyalty points they get fetched and added
+        data = es.sql_select(f"SELECT points FROM loyalty WHERE id = '{user.id}'")
+        prevpoints = int(data[0][0])
+        print(prevpoints)
+        es.sql_exec(f"UPDATE loyalty SET points = {points + prevpoints} WHERE id = '{user.id}'")
+
+        await interaction.followup.send(
+            f"{user.mention} got {points} loyalty points, they now have {prevpoints + points} loyalty points in total!")
+            
+    @app_commands.command(name="rewardchannel", description="Rewards all users currently in a channel with loyalty points")
+    @app_commands.describe(channel="All users currently in this channel get Loyalty Points")
+    @app_commands.describe(points="5 points for every participant")
+    async def rewardchannel(self, interaction: discord.Interaction, channel: discord.VoiceChannel, points: int):
+        roles = [1395315033172480000]
+        for role in allowedRoles:
+            roles.append(role)
+        if not await es.checkPerms(interaction, roles): # add event host role
+            return
+
+        if not channel.members:
+            await interaction.response.send_message(f"Nobody is in {channel.mention} right now.")
+            return
+        
+        await interaction.response.send_message( f"{len(channel.members)} users in channel {channel.mention} got {points} loyalty points:" )
+        for member in channel.members:
+            await self._add_loyalty_points(interaction, member, points)
+
+
     @app_commands.command(name="profile", description="Check your Loyalty points and your balance")
     async def profile(self, interaction: discord.Interaction):
         user = interaction.user
